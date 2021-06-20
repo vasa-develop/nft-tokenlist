48463774fba2d53497e6f2d194c96e07a67d4c31
vasa <vasa@signy.io>
2021-06-20 21:56:40 +0530

    initial commit
    License: BUSL-1.1
    Signed-off-by: Vaibhav Saini <vasa.develop@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+## NFT tokenlist
+> A tokenlist for ERC721, ERC1155
+
+This repository implements a mechanism to generate customizable tokenlist using some simple scripts which pull the data from [OpenSea Graphql API](https://api.opensea.io/graphql). 
+
+The scripts follow the [collectibles tokenlist schema from 0xsequence](https://github.com/0xsequence/collectible-lists).
+
+You can find a sample tokenlist supporting 3300 NFTs in [trimmed_3300_nfts.tokenlist.json](./trimmed_3300_nfts.tokenlist.json).
+
+## How NFT tokenlist generation works? [WIP]
+- Use the [`.env`](./.env) file to specify the parameters.
+- Use the parameters to generate the NFT tokenlist (from cached data or opensea endpoint).
---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+var tokens = []
+var numberOfRawDataFiles = 33
+
+for (let i = 0; i < numberOfRawDataFiles; i++) {
+    rawdata = fs.readFileSync('./cached/'+'raw'+i+'.json')
+    rawdata = JSON.parse(rawdata.toString())
+    var list = rawdata.data.collections.edges.map((edge) => { 
+    node = edge.node
+    return {
+        "name": node.name,
+        // "description": node.description,
+        // "owner": node.owner ? node.owner.address : null,
+        "chainId": node.assetContracts.edges.length > 0 
+            ? node.assetContracts.edges[0].node.blockExplorerLink.split('/')[2] == "polygonscan.com" ? 137 : 1
+            : 1,
+        "address": node.assetContracts.edges.length > 0 
+            ? node.assetContracts.edges[0].node.blockExplorerLink.split('/').pop()
+            : "",
+        "standard": node.assetContracts.edges.length > 0 
+            ? node.assetContracts.edges[0].node.tokenStandard.toLocaleLowerCase()
+            : "",
+        "symbol": node.assetContracts.edges.length > 0 
+            ? node.assetContracts.edges[0].node.symbol
+            : null,
+        // "assetCount": node.assetCount,
+        "logoURI": node.imageUrl,
+        // "tags": node.stringTraits
+    }
+    })
+    tokens = [...tokens, ...list]
+}
+
+var final = {
+    "name": "Genie Tokenlist",
+    "timestamp": "2021-06-19T18:53:14.364Z",
+    "version": {
+        "major": 0,
+        "minor": 0,
+        "patch": 0
+    },
+    "tags": {},
+    "logoURI": "blob:https://in.pinterest.com/4eaf7eed-b9e1-45ca-a7ee-6e483b353301",
+    "keywords": ["genie", "erc20", "erc721", "erc1155"],
+    "tokens": tokens
+}
+
+console.log("Number of tokens: ", tokens.length)
+
+fs.writeFileSync("./genie.tokenlist.json", JSON.stringify(final, null, 2));
